a4e717de8f0ae1e0d1eba208fd45d4eadd18e7d3
Fede <fededelveliz2017@gmail.com>
2024-08-30 00:21:31 -0300

    Rama Pedro con los cambios que hizo

---
+++ CapaServicios/ValidarDigito.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaServicios
+{
+    public static class ValidarDigito
+    {
+        public static bool Vdigito(string contraseña)
+        {
+            bool validado = false;
+
+            //lo mismo que con mayusculas/minusculas
+            for (int i = 0; i < contraseña.Length; i++)
+            {
+                if (char.IsDigit(contraseña[i]))
+                {
+                    validado = true;
+                    return validado;
+                }
+            }
+            return validado;
+        }
+    }
+}
---
+++ CapaServicios/ValidarEspecial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaServicios
+{
+    public static class ValidarEspecial
+    {
+        public static bool Vespecial(string contraseña)
+        {
+            bool validado = false;
+            string especiales = "!#$%&/()=*+~?'¡¿-_@,.{}[]¨´`'|°^";
+            int retorno = especiales.Intersect(contraseña).Count();
+
+            if (retorno > 0) { validado = true; }
+
+
+
+            return validado;
+        }
+    }
+}
